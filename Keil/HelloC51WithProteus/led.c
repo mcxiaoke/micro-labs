c02b921560b74cdb53ff7dc239e85d857472a72e
mcxiaoke <mcxiaoke@live.com>
2017-07-20 21:48:18 +0800

    turn on multi leds

--- Keil/HelloC51WithProteus/led.c
+++ Keil/HelloC51WithProteus/led.c
@@ -1,6 +1,9 @@
 #include<reg51.h>
 
-sbit LED = P2^7;
+sbit LED27 = P2^7;
+sbit LED26 = P2^6;
+sbit LED25 = P2^5;
+sbit LED24 = P2^4;
 
 // for 12MHz
 void delay_1s(){
@@ -8,11 +11,24 @@ void delay_1s(){
 	for(i = 98;i > 0; i--)
 	for(j = 101; j > 0; j--)
 	for(k = 49; k > 0; k--);
-}
+} 
+
+void delay_500ms(){
+	unsigned char i,j,k;
+	for(i = 68;i > 0; i--)
+	for(j = 61; j > 0; j--)
+	for(k = 29; k > 0; k--);
+} 
 
 void main(void){
-	while(1){
-		LED = !LED;	
-		delay_1s();		
-	}
+	while(1){    
+        LED27=!LED27;
+        delay_500ms();	
+        LED26=!LED26;
+        delay_500ms();	
+        LED25=!LED25;
+        delay_500ms();	
+        LED24=!LED24;
+        delay_500ms();
+    }
 }
