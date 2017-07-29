03ad0594dd174dee0a795e9f77d701764093245f
mcxiaoke <mcxiaoke@live.com>
2017-07-29 20:16:03 +0800

    add ds1307 demo2

--- Keil/DS1307_Demo2/demo.c
+++ Keil/DS1307_Demo2/demo.c
@@ -1,38 +1,46 @@
-#include "Includes.h"
 #include "debug.h"
+#include "i2c_a.h"
 #include <stdio.h>
 #include <string.h>
 
 void delay_1s()
 {
     unsigned int i, j, k;
-    for (i = 88; i > 0; i--)
-        for (j = 71; j > 0; j--)
-            for (k = 40; k > 0; k--)
+    for (i = 100; i > 0; i--)
+        for (j = 100; j > 0; j--)
+            for (k = 60; k > 0; k--)
                 ;
 }
 
+
+unsigned char a1,a2,a3;
+unsigned char a4,a5,a6,a7;
+char buf[64];
 void read()
 {
-    char buf[16] = { 0 };
-    unsigned char rtc[8];
-    unsigned char* d;
-    unsigned char* t;
-    TI = 1;
-    d = Get_DS1307_RTC_Date();
-    printf("%d/%d/%d ", (int)d[3], (int)d[2], (int)d[1]);
-    memset(buf, 0, sizeof(buf));
-    t = Get_DS1307_RTC_Time();
-    printf("%d:%d:%d\n", (int)t[2], (int)t[1], (int)t[0]);
+    dLog("Begin Read:");
+    InitI2c();
+    a1 = read_byte(0x0);
+    a2 = read_byte(0x1);
+    a3 = read_byte(0x2);
+    a4 = read_byte(0x3);
+    a5 = read_byte(0x4);
+    a6 = read_byte(0x5);
+    a7 = read_byte(0x6);
+    memset(buf,0,sizeof(buf));
+    sprintf(buf, "%x %x %x %x %x %x %x",
+        (int)1,(int)2,(int)3,
+        (int)4,(int)5,(int)6,(int)7);
+    dLog(buf);
+    dLog(":End Read");
 }
 
 void main()
 {
     dInit();
-    dLog("Initialized.\n");
-    InitI2C();
+    dLog("Hello,World!\n");
     while (1) {
-        delay_1s();
         read();
+        delay_1s();
     }
 }
