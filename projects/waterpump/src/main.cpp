bbcab2ba3b36968b6ae279d6386a5f08a15aa07f
mcxiaoke <mcxiaoke@live.com>
2019-10-01 23:26:28 +0800

    water pump: disable blynk

--- projects/waterpump/src/main.cpp
+++ projects/waterpump/src/main.cpp
@@ -682,7 +682,7 @@ void setup() {
   ntpSetup();
   ntpUpdate();
   setTimers();
-  Blynk.begin(blynkAuth, ssid, pass);
+//   Blynk.begin(blynkAuth, ssid, pass);
   // You can also specify server:
   // Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
   // Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
@@ -692,7 +692,7 @@ void setup() {
 }
 
 void loop() {
-  Blynk.run();
+//   Blynk.run();
   timer.run();
 }
 
